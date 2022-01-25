64d965ab6101a23b4eef80774f9d53388215b21e
Shawn-Fries <samfoley275@gmail.com>
2022-01-25 15:19:25 -0500

    finished two sum

--- skeleton/phase_1_arrays.js
+++ skeleton/phase_1_arrays.js
@@ -1,3 +1,5 @@
+// Uniq
+
 Array.prototype.uniq = function () {
     let charExists  = {};
     let result = [];
@@ -14,4 +16,30 @@ Array.prototype.uniq = function () {
 // }
 
 console.log([1,1,2,2,3,3,4,4,5,5].uniq())
+
+
+// Two Sum
+
+Array.prototype.twoSum = function() {
+    let result = [];
+    let that = this;
+
+    //debugger;
+   this.forEach(function (ele1, index1) {
+        that.forEach(function (ele2, index2) {
+            if (index2 > index1 && ele1 + ele2 === 0) {
+                result.push([ele1, ele2]);
+            }
+        })
+   })
+
+    // self.forEach(function (ele1, index1) {
+    //     self.forEach(function (ele2, index2) {
+    //         if (index2 > index1 && ele1 + ele2 === 0) {
+    //             result.push([ele1, ele2]);
+    //         }
+    //     })
+    // })
+    return result;
+}
 
